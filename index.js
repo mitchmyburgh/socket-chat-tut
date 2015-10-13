a4cd75a02e9bc05014ccceb15b57d8364e676558
Mitch Myburgh <mitchmyburgh@gmail.com>
2015-10-13 13:24:32 +0200

    fixed username

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var io = require('socket.io')(http)
 //for production
 //http://www.jamie-white.com/html5/setting-up-socket-io-for-production/
 
+//people object for storing username
+var people = {};
+
 //express app
 app.get('/', function (req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -12,10 +15,23 @@ app.get('/', function (req, res){
 
 //handle socket connection 
 io.on('connection', function (socket){
-	console.log('user connected');
+	console.log('user connected on socket: '+socket.id);
+	
+	//broadcast on connect
+	socket.broadcast.emit('chat message', "User connected");
+
 	//handle disconnect
 	socket.on('disconnect', function () {
-		console.log('user disconnected')
+		console.log('user disconnected');
+		
+		//broadcast on disconnect
+		io.emit('chat message', 'User disconnected');
+	});
+
+	//handle join
+	socket.on('join', function (name){
+		people[socket.id] = name;
+		io.sockets.emit('chat message', "User "+ name +" connected");
 	});
 
 	//handle chat message
