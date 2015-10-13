96666a2483d9725459d22d66628ac9c075a0a0b8
Mitch Myburgh <mitchmyburgh@gmail.com>
2015-10-13 11:40:52 +0200

    finished tutorial

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http)
 
+//for production
+//http://www.jamie-white.com/html5/setting-up-socket-io-for-production/
+
+//express app
 app.get('/', function (req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+//handle socket connection 
 io.on('connection', function (socket){
-	console.log('user connected on socket: '+socket);
+	console.log('user connected');
+	//handle disconnect
+	socket.on('disconnect', function () {
+		console.log('user disconnected')
+	});
+
+	//handle chat message
+	socket.on('chat message', function (msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});	
 });
 
+//setup the server to listen
 http.listen(3000, function () {
 	console.log('listening on *:3000');
+});
+
+//get input and broadcast it
+var stdin = process.openStdin();
+stdin.addListener("data", function(d) {
+	console.log("broadcasting message: "+d.toString().trim());
+	io.emit('chat message', d.toString().trim());
 });
