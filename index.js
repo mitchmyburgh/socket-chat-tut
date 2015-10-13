8d542eb134ef91b78b2dc1fe4fc0a54206edb460
Mitch Myburgh <mitchmyburgh@gmail.com>
2015-10-13 20:22:54 +0200

    added private messaging

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var io = require('socket.io')(http)
 //for production
 //http://www.jamie-white.com/html5/setting-up-socket-io-for-production/
 
-//people object for storing username
-var people = {};
-
 //express app
 app.get('/', function (req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -16,7 +13,10 @@ app.get('/', function (req, res){
 //handle socket connection 
 io.on('connection', function (socket){
 	console.log('user connected on socket: '+socket.id);
-	
+	for (var user_soc in io.sockets.sockets){ //for efficiency rather make this a json object and send it
+		console.log(io.sockets.sockets[user_soc].nickname);
+		socket.emit('online users', io.sockets.sockets[user_soc].nickname);
+	}
 	//broadcast on connect
 	socket.broadcast.emit('chat message', "User connected");
 
@@ -38,7 +38,31 @@ io.on('connection', function (socket){
 	socket.on('chat message', function (msg){
 		console.log('message: ' + msg);
 		socket.broadcast.emit('chat message',socket.nickname+' '+ msg);
+	});
+
+	//handle username change
+	socket.on('change username', function (name){		
+		io.emit('chat message', socket.nickname+" changed to "+name);
+		socket.nickname = name;
 	});	
+	
+	//show user typing
+	socket.on('typing', function (msg){
+		socket.broadcast.emit('typing', socket.nickname+" "+msg);
+	});
+
+	//private message
+	socket.on('private message', function (msg){
+		for (var user_soc in io.sockets.sockets){ //rather store user
+			if (io.sockets.sockets[user_soc].nickname === msg.name){
+				console.log(user_soc);
+				console.log(msg.message);
+				socket.broadcast.to(io.sockets.sockets[user_soc].id).emit('private message', msg.message+" (private)")
+			} 
+			//console.log(io.sockets.sockets[user_soc].nickname);
+			//socket.emit('online users', io.sockets.sockets[user_soc].nickname);
+		}
+	});
 });
 
 //setup the server to listen
